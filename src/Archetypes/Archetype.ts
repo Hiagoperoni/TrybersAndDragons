302652f82e568add37de7d6c926325a179ef3e83
HiagoP <hiagoperonip@gmail.com>
2023-02-16 18:36:54 -0300

    Requisito 04 pronto

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,31 @@
+abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(protected nome: string) {
+    this._name = nome;
+    this._special = 0;
+    this._cost = 0; 
+  }
+
+  public get name(): string {
+    return this.nome;
+  }
+
+  public get special(): number {
+    return this._special;
+  }
+
+  public get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): void {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): string;
+}
+
+export default Archetype;
